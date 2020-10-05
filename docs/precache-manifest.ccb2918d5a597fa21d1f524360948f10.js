b57d3932894d473feba10a220ff2814de69370ce
Ivan Castellano <i.casterrez@gmail.com>
2020-10-05 10:59:42 -0300

    docs agregado

---
+++ docs/precache-manifest.ccb2918d5a597fa21d1f524360948f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023e43ece25934483508e580e67b376a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b62b518afa049f19ee",
+    "url": "/static/css/main.832c6efd.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b62b518afa049f19ee",
+    "url": "/static/js/main.7b0c0f47.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
